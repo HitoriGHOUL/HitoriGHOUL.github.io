04a629a797f365df300ea9f0acba3fd8d7d275e0
Alain Chau <alain06chau3214@gmail.com>
2023-12-03 00:55:32 -0800

    mobile

--- scripts.js
+++ scripts.js
@@ -9,11 +9,15 @@ function toggleMobileButton(){
         for (var x = 0; x < dropDownContent.length; x++) {
             if (dropDownContent[x].style.display == "block"){
                 dropDownContent[x].style.display = "none";
-                dropBtn[x].innerHTML = "O"
+                dropBtn[x].innerHTML = "O";
+                dropBtn[x].style.backgroundColor = "black";
+                dropBtn[x].style.color = "white";
             }
             else{
                 dropDownContent[x].style.display = "block";
                 dropBtn[x].innerHTML = "X"
+                dropBtn[x].style.backgroundColor = "white";
+                dropBtn[x].style.color = "black";
             }
         }
 
